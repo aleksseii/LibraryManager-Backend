bc70f6c8e27218bbe966e23e30d556f37e681a4c
aleksseii <alesha.shkel@mail.ru>
2022-07-19 18:27:29 +0300

    added running scripts and running console

--- src/main/java/ru/aleksseii/Application.java
+++ src/main/java/ru/aleksseii/Application.java
@@ -1,8 +1,11 @@
 package ru.aleksseii;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
@@ -11,6 +14,10 @@ public class Application {
 
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
-        context.close();
+        try {
+            Console.main(args);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
